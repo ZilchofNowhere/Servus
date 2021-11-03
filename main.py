68837e610b7fbd883272089463ba3d645e5c3d0f
ZilchofNowhere <81156765+ZilchofNowhere@users.noreply.github.com>
2021-11-03 22:13:39 +0300

    hopefully the last fix to exec

--- main.py
+++ main.py
@@ -173,8 +173,8 @@ async def info(ctx):
 async def exec(ctx, *, arg):
     os.system(f"{arg} > ./exec.txt")
     with open("./exec.txt") as exec:
-        for l in exec.readlines():
-            await ctx.send(l)
+
+        await ctx.send(exec.read())
 
 #bitiş
 client.run("ODIzNDk4NTMxNzI4Nzg1NDE4.YFhsxQ.uIrEf-YC0FAZG5_c9m_C3mm75Y4")
